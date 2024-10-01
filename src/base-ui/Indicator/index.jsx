24dcd3c9db1da2441e812e22820892f62e1a68ae
Deng <3102934816@qq.com>
2024-10-01 19:48:59 +0800

    点击跳转详情页面

--- src/base-ui/Indicator/index.jsx
+++ src/base-ui/Indicator/index.jsx
@@ -6,15 +6,21 @@ const Indicator = memo((props) => {
   const scrollRef = useRef()
 
   useEffect(() => {
+    // 1 获取selectIndex对应的item
     const selectItemEl = scrollRef.current.children[selectIndex]
     const selectItemWidth = selectItemEl.clientWidth
     const selectItemOffset = selectItemEl.offsetLeft
 
+    // 2 content的宽度
     const scrollElWidth = scrollRef.current.clientWidth
     const scrollElScroll = scrollRef.current.scrollWidth
 
+    // 3 滚动的距离
     let distance = selectItemWidth * 0.5 + selectItemOffset - scrollElWidth * 0.5
+    
+    // 左边特殊情况处理
     if (distance < 0) distance = 0
+    // 右边特殊情况处理
     if (distance > scrollElScroll - scrollElWidth) distance = scrollElScroll - scrollElWidth
     scrollRef.current.style.transform = `translate(${-distance}px)`
   }, [selectIndex])
--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -10,63 +10,78 @@ import Indicator from '@/base-ui/Indicator';
 import classNames from 'classnames';
 
 const RoomItem = memo((props) => {
-  const { itemData, itemWidth = "33.3333%"} = props
+  const { itemData, itemWidth = "33.3333%", itemClick} = props
   const [selectIndex, setSelectIndex] = useState(0)
   const swiperRef = useRef()
 
-  function controlClickHandle(isNext = true) {
-    if (isNext) swiperRef.current.next()
-    else swiperRef.current.prev()
-
+  function controlClickHandle(isNext = true, event) {
+    isNext ? swiperRef.current.next() : swiperRef.current.prev()
     let newIndex = isNext ? selectIndex + 1: selectIndex - 1
     if (newIndex < 0) newIndex = itemData.picture_urls.length - 1
     if (newIndex > itemData.picture_urls.length - 1) newIndex = 0
     setSelectIndex(newIndex)
+
+    // 阻止事件冒泡
+    event.stopPropagation()
+
   }
 
+  function handleItemClick() {
+    if (itemClick) itemClick()
+
+  }
+
+  // 子元素
+  const pictureElement = (
+    <div className='cover'>
+      <img src={itemData.picture_url} alt="" />
+    </div>
+  )
+
+  const sliderElenment = (
+    <div className='slider'>
+      <div className='control'>
+        <div className="btn left" onClick={e => controlClickHandle(false, e)}>
+          <IconArrowLeft width="24" height="24"/>
+        </div>
+        <div className="btn right" onClick={e => controlClickHandle(true, e)}>
+          <IconArrowRight width="24" height="24"/>
+        </div>
+      </div>
+
+      <div className='indicator'>
+        <Indicator selectIndex={selectIndex}>
+          {
+            itemData.picture_urls?.map((item, index) => {
+              return (
+                <div className='item' key={item}>
+                  <span className={classNames("dot", {active: selectIndex === index})}></span>
+                </div>
+              )
+            })
+          }
+        </Indicator>
+      </div>
+      {/* dots是否显示默认的面板指示点 */}
+      <Carousel dots={false} ref={swiperRef}>
+        {/* 放列表数据 */}
+        {
+          itemData.picture_urls?.map((item, index) => {
+            return (
+              <div key={index} className="cover">
+                <img src={item} alt="" />
+              </div>
+            )
+          })
+        }
+      </Carousel>
+    </div>
+  )
 
   return (
-    <ItemWrapper itemWidth={itemWidth} >
+    <ItemWrapper itemWidth={itemWidth} onClick={handleItemClick}>
       <div className='inner'>
-        {
-          !itemData.picture_urls ? <div className='cover'>
-          <img src={itemData.picture_url} alt="" />
-        </div> :
-        <div className='slider'>
-          <div className='control'>
-            <div className="btn left" onClick={e => controlClickHandle(false)}>
-              <IconArrowLeft width="24" height="24"/>
-            </div>
-            <div className="btn right" onClick={e => controlClickHandle(true)}>
-              <IconArrowRight width="24" height="24"/>
-            </div>
-          </div>
-          <div className='indicator'>
-            <Indicator selectIndex={selectIndex}>
-              {
-                itemData.picture_urls.map((item, index) => {
-                  return (
-                    <div className='item' key={item}>
-                      <span className={classNames("dot", {active: selectIndex === index})}></span>
-                    </div>
-                  )
-                })
-              }
-            </Indicator>
-          </div>
-          <Carousel dots={false} ref={swiperRef}>
-            {
-              itemData.picture_urls.map((item, index) => {
-                return (
-                  <div key={index} className="cover">
-                    <img src={item} alt="" />
-                  </div>
-                )
-              })
-            }
-          </Carousel>
-        </div>
-        }
+        { !itemData.picture_urls ? pictureElement : sliderElenment }
         <div className='desc'>{itemData.verify_info.messages.join("·")}</div>
         <div className='name'>{itemData.name}</div>
         <div className='price'>¥{itemData.price}/晚</div>
