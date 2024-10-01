ab624e6982412b2f8e22869b1526098d6ca7b118
Deng <3102934816@qq.com>
2024-10-01 09:57:13 +0800

    r-commit

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -4,18 +4,26 @@ import React, { memo, useEffect, useRef, useState } from 'react'
 import { ScrollWrapper } from './style'
 
 const ScrollView = memo((props) => {
-  /** 记录正在显示的是哪一个 */
+  /** 记录正在显示的是哪一个按钮 */
+
+  // 记录item索引，偏移量即为索引对应的item的偏移量
   const [posIndex, setPosIndex] = useState(0)
   const [showLeft, setShowLeft] = useState(false)
   const [showRight, setShowRight] = useState(true)
 
   /** 滚动区域的值 */
+  // 使用useRef获取不需要渲染的值
   const scrollRef = useRef()
   const totalDistanceRef = useRef(0)
+
+  // 组件渲染完后，判断内容是否溢出，溢出就显示右侧的按钮
   useEffect(() => {
+    // 可滚动区域宽度
     const scrollWidth = scrollRef.current.scrollWidth
+    // 内容显示出的宽度
     const clientWidth = scrollRef.current.clientWidth
     totalDistanceRef.current = scrollWidth - clientWidth
+    // 按钮显示条件
     setShowRight(totalDistanceRef.current > 0)
   }, [props.children])
 
@@ -31,11 +39,14 @@ const ScrollView = memo((props) => {
   function scrollPosition(index) {
     const scrollLeft = scrollRef.current.children[index].offsetLeft
     scrollRef.current.style.transform = `translate(-${scrollLeft}px)`
+    // 偏移之后计算一下最新索引
     setPosIndex(index)
+    // 是否继续显示右侧按钮
     if (scrollLeft > totalDistanceRef.current) {
       setShowRight(false)
     }
     setShowRight(totalDistanceRef.current > scrollLeft)
+    // 只要已经滚动的区域大于0，就显示左侧按钮
     setShowLeft(scrollLeft > 0)
   }
 
@@ -51,6 +62,7 @@ const ScrollView = memo((props) => {
           <IconArrowRight/>
         </div>
       )}
+      {/* 内容可变，采用插槽 */}
       <div className='content'>
         <div className='scroll' ref={scrollRef}>
           {props.children}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { ThemeProvider } from "styled-components"
+
 import theme from "./assets/theme"
 import App from './App';
 import "normalize.css"
-import "@/assets/css/index.less"
 import "antd/dist/antd.less"
+import "@/assets/css/index.less"
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
